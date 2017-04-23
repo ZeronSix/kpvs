fe9f124a8d5ee24be746d4ebf513fa7a53b750f2
Vyacheslav Zhdanovskiy <zeronsix@gmail.com>
2017-04-24 01:16:48 +0300

    Refactor port.hpp
    Replace pointers with references and bitmask with std::array for
    better C++ usage.
    
    Signed-off-by: Vyacheslav Zhdanovskiy <zeronsix@gmail.com>
    Signed-off-by: Philippe Mikoyan <phila775@gmail.com>

--- include/port.hpp
+++ include/port.hpp
@@ -2,7 +2,6 @@
 #define __KPVS_PORT_HPP_
 
 #include <array>
-#include <bitset>
 #include "pin.hpp"
 
 namespace kpvs
@@ -11,12 +10,13 @@ namespace kpvs
     class Port
     {
     public:
-        Port(Pin::PinMode s = Pin::PINMODE_IN);
-        uint64_t getModes () const;
-        int      setModes (uint64_t mask);
-        uint64_t getStates() const;
-        int      setStates(uint64_t mask);
-        Pin*     getPin(std::size_t n);
+        Port(Pin::PinMode s = Pin::PINMODE_HI_Z);
+        std::array<Pin::PinMode, W> getModes() const;
+        std::array<VoltageState, W> getStates() const;
+
+        int  setModes(std::array<Pin::PinMode, W> modes);
+        int  setStates(std::array<VoltageState, W> mask);
+        Pin& getPin(std::size_t n);
     private:
         std::array<Pin, W> pins;
     };
@@ -30,71 +30,32 @@ namespace kpvs
     }
 
     template <Width W>
-    uint64_t Port<W>::getStates() const
+    std::array<VoltageState, W> Port<W>::getStates() const
     {
         static_assert(VSTATE_LOW  == 0b0, "Constant value is wrong!");
         static_assert(VSTATE_HIGH == 0b1, "Constant value is wrong!");
-        std::bitset<W> psm(0);           //Pin state mask
+        std::array<VoltageState, W> states;
 
         int cnt = 0;
-        
         for (auto& pin : pins)
         {
-            psm[cnt] = pin.state;
+            states.at(cnt) = pin.state;
             cnt++;
         }
 
-        return psm.to_ullong();
+        return states;
     }
 
     template <Width W>
-    int Port<W>::setStates(uint64_t psMask)
+    int Port<W>::setStates(std::array<VoltageState, W> states)
     {
         static_assert(VSTATE_LOW  == 0b0, "Constant value is wrong!");
         static_assert(VSTATE_HIGH == 0b1, "Constant value is wrong!");
-        std::bitset<W> psm(psMask);      // Pin state mask
 
         std::size_t cnt = 0;
-        
         for (auto& pin : pins)
         {
-            pin.state = (VoltageState)psm.test(cnt);
-            cnt++;
-        }
-
-        return ERR_OK;
-    }
-    
-    template <Width W>
-    uint64_t Port<W>::getModes() const
-    {
-        static_assert(Pin::PINMODE_IN  == 0b0, "Constant value is wrong!");
-        static_assert(Pin::PINMODE_OUT == 0b1, "Constant value is wrong!");
-        std::bitset<W> pmm(0);           // Pin mode mask
-
-        std::size_t cnt = 0;
-        
-        for (auto& pin : pins)
-        {
-            pmm[cnt] = pin.mode;
-            cnt++;
-        }
-
-        return pmm.to_ullong();
-    }
-
-    template <Width W>
-    int Port<W>::setModes(uint64_t pmMask)
-    {
-        static_assert(Pin::PINMODE_IN  == 0b0, "Constant value is wrong!");
-        static_assert(Pin::PINMODE_OUT == 0b1, "Constant value is wrong!");
-        std::bitset<W> pmm(pmMask);      // Pin mode mask
-
-        std::size_t cnt = 0;
-        
-        for (auto& pin : pins)
-        {
-            pin.mode = (Pin::PinMode)pmm.test(cnt);
+            pin.state = states.at(cnt);
             cnt++;
         }
 
@@ -102,9 +63,42 @@ namespace kpvs
     }
 
     template <Width W>
-    Pin* Port<W>::getPin(std::size_t n)
+    std::array<Pin::PinMode, W> Port<W>::getModes() const
     {
-        return &pins[n];
+        static_assert(Pin::PINMODE_IN  == 0b0, "Constant value is wrong!");
+        static_assert(Pin::PINMODE_OUT == 0b1, "Constant value is wrong!");
+        std::array<Pin::PinMode, W> modes;
+
+        std::size_t cnt = 0;
+        for (auto& pin : pins)
+        {
+            modes.at(cnt) = pin.mode;
+            cnt++;
+        }
+
+        return modes;
+    }
+
+    template <Width W>
+    int Port<W>::setModes(std::array<Pin::PinMode, W> modes)
+    {
+        static_assert(Pin::PINMODE_IN  == 0b0, "Constant value is wrong!");
+        static_assert(Pin::PINMODE_OUT == 0b1, "Constant value is wrong!");
+
+        std::size_t cnt = 0;
+        for (auto& pin : pins)
+        {
+            pin.mode = modes.at(cnt);
+            cnt++;
+        }
+
+        return ERR_OK;
+    }
+
+    template <Width W>
+    Pin& Port<W>::getPin(std::size_t n)
+    {
+        return pins.at(n);
     }
 }
 
